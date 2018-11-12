74d169687f40324bf24fbbf57ba962fb25d6f7d5
Cam Sexton <cameron.t.sexton@gmail.com>
2018-11-12 10:46:47 -0800

    add head to page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,23 +1,31 @@
+import Head from 'next/head';
+
 const Index = () => (
-  <div className="logo_header">
-    <img className="logo" src="static/images/mot-logo.svg"/>
-    <h1 className="logo_title"> LA Metro - Performance Monitor</h1>
-    <style jsx>{`
-      .logo_header {
-        display: flex;
-        align-items: center;
-        width: 100%;
-      }
-      .logo_title {
-        display: inline;
-        font-weight: 200;
-      }
-      .logo {
-        display: inline;
-        width: 75px;
-        padding: 10px;
-      }  
-    `}</style>
+  <div>
+    <Head>
+      <title>Metro OnTime</title>
+      <link rel="icon" type="image/x-icon" href="static/favicon.ico" />
+    </Head>
+    <div className="logo_header">
+      <img className="logo" src="static/images/mot-logo.svg"/>
+      <h1 className="logo_title"> LA Metro - Performance Monitor</h1>
+      <style jsx>{`
+        .logo_header {
+          display: flex;
+          align-items: center;
+          width: 100%;
+        }
+        .logo_title {
+          display: inline;
+          font-weight: 200;
+        }
+        .logo {
+          display: inline;
+          width: 75px;
+          padding: 10px;
+        }  
+      `}</style>
+    </div>
   </div>
 )
 
