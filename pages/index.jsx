0bcb00021eab8c5cf8c84d98a1c0a407fa7ae0fb
Cam Sexton <cameron.t.sexton@gmail.com>
2018-11-14 00:57:01 -0800

    add explanation text

--- pages/index.jsx
+++ pages/index.jsx
@@ -34,7 +34,11 @@ const Index = () => (
       <img className="logo" src="static/images/mot-logo.svg" alt=""/>
       <h1 className="logo_title">LA Metro - Performance Monitor</h1>
     </div>
-    <h2 className="subtitle">Sample Marey Diagram</h2>
+    <h2 className="subtitle">Logged Trains compared to the Schedule</h2>
+    <p>This diagram shows 3 hours of trains running northbound on the Gold line between 6am & 9am on October 30, 2018.<br/>
+      Light grey lines show the schedule, while blue lines are the actual paths trains took during that time period,<br/>
+      at least according to vehicle position data logged once per minute from NextBus.
+    </p>
     <DataLoader />
     <style jsx>{`
       .logo_header {
