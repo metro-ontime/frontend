1bd1d481275cbdf6e778c1e96f65df4d018b5a62
Nick Loui <nick@civicfeed.com>
2019-02-12 20:21:19 -0800

    times

--- components/DataParser.jsx
+++ components/DataParser.jsx
@@ -5,7 +5,6 @@ import Highchart from './charts/Highchart';
 import LinearIndeterminate from './LinearIndeterminate';
 
 class DataParser extends Component {
-
   constructor(props) {
     super(props);
     this.state = { trips: null };
@@ -14,8 +13,12 @@ class DataParser extends Component {
 
   componentDidMount() {
     // Should be run as web worker?
-    var csvFilePath = "../static/sample_data/trips_latest.csv";
-    DataFrame.fromCSV(`https://s3-us-west-1.amazonaws.com/h4la-metro-performance/data/vehicle_tracking/processed/${this.props.line}_lametro-rail/2019-01-30.csv`).then(df => {
+    const csvFilePath = '../static/sample_data/trips_latest.csv';
+    DataFrame.fromCSV(
+      `https://s3-us-west-1.amazonaws.com/h4la-metro-performance/data/vehicle_tracking/processed/${
+        this.props.line
+      }_lametro-rail/2019-01-30.csv`,
+    ).then((df) => {
       df = df.cast('relative_position', Number);
       df = df.cast('direction_id', Number);
       let direction0 = df.where(row => row.get('direction_id') == 0);
@@ -25,8 +28,10 @@ class DataParser extends Component {
       direction1 = direction1.groupBy('trip_id').toCollection();
 
       const collectTrip = (trip) => {
-        const theTrip = trip.group.toCollection().map((row) => [row.relative_position, new Date(row.datetime).getTime()]);
-        return { data: theTrip }
+        const theTrip = trip.group
+          .toCollection()
+          .map(row => [row.relative_position, new Date(row.datetime).getTime()]);
+        return { data: theTrip };
       };
 
       const trips0 = direction0.map(collectTrip);
@@ -36,28 +41,23 @@ class DataParser extends Component {
   }
 
   updateTrips(trips0, trips1) {
-    this.setState({trips: [trips0, trips1]});
+    this.setState({ trips: [trips0, trips1] });
   }
 
   render() {
     return (
       <div>
-        { this.state.trips ? 
-            <div>
-              { this.props.direction == 0 && (
-                <Highchart data={ this.state.trips[0] }/>
-              )}
-              { this.props.direction == 1 && (
-                <Highchart data={ this.state.trips[1] }/>
-              )}
-            </div>
-            :
-            <LinearIndeterminate />
-        }
+        {this.state.trips ? (
+          <div>
+            {this.props.direction == 0 && <Highchart data={this.state.trips[0]} />}
+            {this.props.direction == 1 && <Highchart data={this.state.trips[1]} />}
+          </div>
+        ) : (
+          <LinearIndeterminate />
+        )}
       </div>
-    )
+    );
   }
 }
 
 export default DataParser;
-
