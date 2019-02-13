c6ba46a02631a8cc1f96e1bb6d4e0e4884c3ab17
Cam Sexton <cameron.t.sexton@gmail.com>
2019-02-12 17:20:42 -0800

    convert TrainDetails to stateful component with Direction state

--- pages/line/TrainDetails.jsx
+++ pages/line/TrainDetails.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Typography, Grid, Toolbar, Paper, Divider } from '@material-ui/core';
 import Highchart from '../../components/charts/Highchart';
 import DataParser from '../../components/DataParser';
@@ -17,32 +17,41 @@ const styles = theme => ({
   }
 });
 
-const TrainDetails = (props) => {
-  const { classes } = props;
-  return (
-    <Grid container spacing={40}>
-      <Grid item xs={12}>
-        <Paper elevation={1} className={ classes.paper }>
-          <Typography variant="h2">Trip Visualizer</Typography>
-          <Divider className={ classes.root } />
-          <Typography variant="body1" component="p">
-            Grey lines depict scheduled train journeys, while colored lines show the observed paths of trains over this time frame. Colored lines above grey lines depict early trains and colored lines below grey lines depict late trains.
-          </Typography>
-      </Paper>
-      </Grid>
+class TrainDetails extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      direction: '0'
+    }
+  }
 
-      <Grid container justify="center" alignItems="center" spacing={24}>
+  render() {
+    const { classes } = this.props;
+    return (
+      <Grid container spacing={40}>
         <Grid item xs={12}>
-          <Toolbar color="primary">
-            <SimpleMenu label="Select Direction: " menuItems={['Azusa / APU', 'Atlantic']} />
-          </Toolbar>
+          <Paper elevation={1} className={ classes.paper }>
+            <Typography variant="h2">Trip Visualizer</Typography>
+            <Divider className={ classes.root } />
+            <Typography variant="body1" component="p">
+              Grey lines depict scheduled train journeys, while colored lines show the observed paths of trains over this time frame. Colored lines above grey lines depict early trains and colored lines below grey lines depict late trains.
+            </Typography>
+        </Paper>
         </Grid>
-        <Grid item xs={12}>
-          <DataParser line={ props.line }/>
+
+        <Grid container justify="center" alignItems="center" spacing={24}>
+          <Grid item xs={12}>
+            <Toolbar color="primary">
+              <SimpleMenu label="Select Direction: " menuItems={['1', '0']} />
+            </Toolbar>
+          </Grid>
+          <Grid item xs={12}>
+            <DataParser line={ this.props.line } direction={ this.state.direction }/>
+          </Grid>
         </Grid>
       </Grid>
-    </Grid>
-  )
-};
+    )
+  }
+}
 
 export default withStyles(styles)(TrainDetails);
